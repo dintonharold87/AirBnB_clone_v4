959f62f7f1d9233f8450c1c3faccb6748ec5923b
Dinton Harold Ainemukama <dintonharold@gmail.com>
2023-01-29 00:46:18 +0300

    Added task 5

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,7 +1,16 @@
 window.addEventListener('load', function () {
+  // task 3
+  $.ajax('http://0.0.0.0:5001/api/v1/status').done(function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+
   // task 2
   const amenityIds = {};
-  $('input[type=checkbox]').change(function () {
+  $('input[type=checkbox]').click(function () {
     if ($(this).prop('checked')) {
       amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
     } else if (!$(this).prop('checked')) {
